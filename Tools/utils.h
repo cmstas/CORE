028d0a73199e39be555a8f7bce67db6b51bc38d8
wsicheng <sicheng@physics.ucsb.edu>
2018-06-16 02:15:12 -0700

    Add faster deltaR check methods to utilities

--- Tools/utils.h
+++ Tools/utils.h
@@ -8,4 +8,9 @@ float DeltaR(LorentzVector v1, LorentzVector v2);
 float DeltaPhi(float phi1, float phi2);
 float MT(float pt1, float phi1, float pt2, float phi2);
 
+namespace utils {
+  bool isCloseObject(const LorentzVector p1, const LorentzVector p2, const float conesize = 0.3);
+  void fastOverlapRemoval(std::vector<LorentzVector>& p4js, const std::vector<LorentzVector>& p4ls, const float conesize = 0.1);
+}
+
 #endif
