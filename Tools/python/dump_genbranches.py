cff59af9c804e7f6bf0481c68e18f20fa43415ee
Nick Amin <amin.nj@gmail.com>
2018-12-11 10:43:04 -0800

    fixed plotter. genweights are validated :tada:

--- Tools/python/dump_genbranches.py
+++ Tools/python/dump_genbranches.py
@@ -11,7 +11,7 @@ class GenWeightChecker(Looper):
         self.nominal = "genps_weight"
         self.hists = {}
         for name in self.toplot:
-            self.hists[name] = r.TH1F(name,"{}/{}".format(name,self.nominal),100,0.0,3.0)
+            self.hists[name] = r.TH1F(name,"{}".format(name,self.nominal),100,0.0,3.0)
 
     def after_loop(self):
         os.system("mkdir -p plots")
@@ -25,13 +25,14 @@ class GenWeightChecker(Looper):
 
     def process_event(self, ievent):
         for name in self.toplot:
-            val = getattr(r.cms3,name)()/getattr(r.cms3,self.nominal)()
+            val = getattr(r.cms3,name)()
             self.hists[name].Fill(val)
 
 if __name__ == "__main__":
 
     ch = r.TChain("Events")
     ch.Add("/hadoop/cms/store/group/snt/run2_mc2018/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1_MINIAODSIM_CMS4_V10-02-02/merged_ntuple_1.root")
+    # ch.Add("/hadoop/cms/store/group/snt/run2_mc2018//QCD_Pt-15to20_EMEnriched_TuneCP5_13TeV_pythia8_RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v2_MINIAODSIM_CMS4_V10-02-02//merged_ntuple_11.root")
 
     looper = GenWeightChecker(ch)
     looper.loop()
